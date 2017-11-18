1e179badd3ebd10b635c1854b8ed00776f63cce8
Yixf(Yi Xianfu) <yixfgit@gmail.com>
2017-11-18 13:38:50 +0800

    Rename output of programming

--- programming/compile.sh
+++ programming/compile.sh
@@ -1,8 +1,8 @@
-cat snippets/header_ioslides.Rmd snippets/body.Rmd > conda_ioslides.Rmd
-Rscript -e "rmarkdown::render('conda_ioslides.Rmd')"
+cat snippets/header_ioslides.Rmd snippets/body.Rmd > programming_ioslides.Rmd
+Rscript -e "rmarkdown::render('programming_ioslides.Rmd')"
 
-cat snippets/header_slidy.Rmd snippets/body.Rmd > conda_slidy.Rmd
-Rscript -e "rmarkdown::render('conda_slidy.Rmd')"
+cat snippets/header_slidy.Rmd snippets/body.Rmd > programming_slidy.Rmd
+Rscript -e "rmarkdown::render('programming_slidy.Rmd')"
 
-cat snippets/header_beamer.Rmd snippets/body.Rmd > conda_beamer.Rmd
-Rscript -e "rmarkdown::render('conda_beamer.Rmd')"
+cat snippets/header_beamer.Rmd snippets/body.Rmd > programming_beamer.Rmd
+Rscript -e "rmarkdown::render('programming_beamer.Rmd')"
